e837be548ce56afd98aa5812ca2dcee422ad119c
ashikcse045 <ashikcse321@gmail.com>
2022-09-28 01:15:48 +0600

    update in footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,15 +1,17 @@
 <footer>
-        <div class="social_icon">
-        <a href="https://www.facebook.com/ashik.cse.vu" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-        <a href="#" target="_blank"><i class="fa-brands fa-youtube"></i></a>
-        <a href="#" target="_blank"><i class="fa-brands fa-github"></i></a>
-        <a href="https://www.linkedin.com/in/ashik-sarker-b7b389237/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-        </div>
-        
-        <div class="dev_info">
-            <div class="inner_div">
-                <p>copyright &copy2022 Thesis/project information registry</p>
-                <p>Devolop by: <strong>Team</strong></p>
+        <div class="footer">
+            <div class="social_icon">
+                <a href="https://www.facebook.com/ashik.cse.vu" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+                <a href="#" target="_blank"><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/ashik-sarker-b7b389237/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+            
+            <div class="dev_info">
+                <div class="inner_div">
+                    <p>copyright &copy2022 Thesis/project information registry</p>
+                    <p>Devolop by: <strong>Team</strong></p>
+                </div>
             </div>
         </div>
-    </footer>
+</footer>
